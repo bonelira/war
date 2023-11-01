1b9aa6eba42ee0350bb3e656cd99548800f8bef8
Leonardo Cavalcanti <leonardocaturyty@hotmail.com>
2023-11-01 11:00:58 -0300

    style: No significant change in style.

--- src/main/java/br/unicap/eng2/debuggin_squad/war/objectives/Message.java
+++
@@ -1,8 +0,0 @@
-package br.unicap.eng2.debuggin_squad.war.objectives;
-
-import br.unicap.eng2.debuggin_squad.war.Player;
-
-public interface Message {
-
-	public String create( String description, String player);
-}
---
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/objectives/message/Message.java
@@ -0,0 +1,8 @@
+package br.unicap.eng2.debuggin_squad.war.objectives.message;
+
+import br.unicap.eng2.debuggin_squad.war.Player;
+
+public interface Message {
+
+	public String create(String description, String player);
+}
