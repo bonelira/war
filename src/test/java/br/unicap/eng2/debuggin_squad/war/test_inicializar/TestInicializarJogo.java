6ead06805cfc0605daed99dd0ee494a5b9790a78
Bone Lira <bone-lira@hotmail.com>
2023-11-02 01:56:16 -0300

    feat: imports para as classes que devem existir

---
+++ src/test/java/br/unicap/eng2/debuggin_squad/war/test_inicializar/TestInicializarJogo.java
@@ -0,0 +1,79 @@
+package br.unicap.eng2.debuggin_squad.war.test_inicializar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/** abaixo estão todas as classes atuais do modelo de domínio e  diagrama de classe */
+// TODO O QUE DEVE SER FEITO:
+// [ ] Criar todas as classes dos imports abaixo
+// [ ] excluir todos os imports que não deveriam estar nessa clase: exp:: RedDice,YellowDice,Attack etc
+// [ ] configurar todos os atributos 
+// [ ] Refatorar classe.
+// [ ] 
+
+import br.unicap.eng2.debuggin_squad.war.GameWar;
+import br.unicap.eng2.debuggin_squad.war.Board;
+import br.unicap.eng2.debuggin_squad.war.Player;
+import br.unicap.eng2.debuggin_squad.war.Territory;
+import br.unicap.eng2.debuggin_squad.war.Army;
+import br.unicap.eng2.debuggin_squad.war.GoalCard;
+import br.unicap.eng2.debuggin_squad.war.TerritoryCard;
+import br.unicap.eng2.debuggin_squad.war.RedDice;
+import br.unicap.eng2.debuggin_squad.war.YellowDice;
+import br.unicap.eng2.debuggin_squad.war.Dealer;
+import br.unicap.eng2.debuggin_squad.war.TurnManager;
+import br.unicap.eng2.debuggin_squad.war.GameRules;
+import br.unicap.eng2.debuggin_squad.war.Attack;
+import br.unicap.eng2.debuggin_squad.war.Battle;
+import br.unicap.eng2.debuggin_squad.war.CardExchange;
+import br.unicap.eng2.debuggin_squad.war.PlayerElimination;
+import br.unicap.eng2.debuggin_squad.war.ConquerPhase;
+import br.unicap.eng2.debuggin_squad.war.MovementPhase;
+import br.unicap.eng2.debuggin_squad.war.EndGamePhase;
+import br.unicap.eng2.debuggin_squad.war.MapGenerator;
+import br.unicap.eng2.debuggin_squad.war.SessionManager;
+import br.unicap.eng2.debuggin_squad.war.RuleManager;
+import br.unicap.eng2.debuggin_squad.war.GameConfigurator;
+import br.unicap.eng2.debuggin_squad.war.GoalConquerContinent;
+import br.unicap.eng2.debuggin_squad.war.GoalConquer24Territory;
+import br.unicap.eng2.debuggin_squad.war.GoalEliminatePlayer;
+import br.unicap.eng2.debuggin_squad.war.Deck;
+import br.unicap.eng2.debuggin_squad.war.Continent;
+import br.unicap.eng2.debuggin_squad.war.Card;
+
+public class TestInicializarJogo {
+    public GameWar CurrentGame = new GameWar();
+
+    public Board board = new Board();
+    public Player player = new Player();
+    public Territory territory = new Territory();
+    public Army army = new Army();
+    public GoalCard goalCard = new GoalCard();
+    public TerritoryCard territoryCard = new TerritoryCard();
+    public RedDice redDice = new RedDice();
+    public YellowDice yellowDice = new YellowDice();
+    public Dealer dealer = new Dealer();
+    public TurnManager turnManager = new TurnManager();
+    public GameRules gameRules = new GameRules();
+    public Attack attack = new Attack();
+    public Battle battle = new Battle();
+    public CardExchange cardExchange = new CardExchange();
+    public PlayerElimination playerElimination = new PlayerElimination();
+    public ConquerPhase conquerPhase = new ConquerPhase();
+    public MovementPhase movementPhase = new MovementPhase();
+    public EndGamePhase endGamePhase = new EndGamePhase();
+    public MapGenerator mapGenerator = new MapGenerator();
+    public SessionManager sessionManager = new SessionManager();
+    public RuleManager ruleManager = new RuleManager();
+    public GameConfigurator gameConfigurator = new GameConfigurator();
+    public GoalConquerContinent goalConquerContinent = new GoalConquerContinent();
+    public GoalConquer24Territory goalConquer24Territory = new GoalConquer24Territory();
+    public GoalEliminatePlayer goalEliminatePlayer = new GoalEliminatePlayer();
+    public Deck deck = new Deck();
+    public Continent continent = new Continent();
+    public Card ard = new Card();
+
+}
