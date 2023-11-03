6f76b591d16359402dc99a3459153de387926cc4
Bruno Vicente <brunohiperbole@gmail.com>
2023-11-03 03:16:05 -0300

    Update testes Territorio

--- src/test/java/br/unicap/nomedisciplina/atividade00/TestTerritorio.java
+++ src/test/java/br/unicap/nomedisciplina/atividade00/TestTerritorio.java
@@ -2,12 +2,13 @@ package br.unicap.nomedisciplina.atividade00;
 
 import org.junit.jupiter.api.Test;
 
+import br.unicap.nomedisciplina.atividade00.player.Jogador;
 import br.unicap.nomedisciplina.atividade00.territorio_controlado.Territorio;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestTerritorio {
-    
+
     
 @Test
 public void testGetNome(){
@@ -15,6 +16,14 @@ public void testGetNome(){
     assertEquals("territorio 1", territorio.getNome());
 }
 
+
+@Test
+public void testSetProprietario(){
+    Territorio territorio = new Territorio ("Territorio 1");
+    Jogador jogador = new Jogador ("Jogador 1");
+    territorio.setProprietario(jogador);
+    assertEquals(jogador, territorio.getProprietario());
+}
 
 
 }
