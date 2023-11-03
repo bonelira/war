798b3e0fffbb74631a1244c0073a13cbffb6edf9
Bruno Vicente <brunohiperbole@gmail.com>
2023-11-03 03:21:00 -0300

    update test Jogador

--- src/test/java/br/unicap/nomedisciplina/atividade00/TestJogador.java
+++ src/test/java/br/unicap/nomedisciplina/atividade00/TestJogador.java
@@ -1,5 +1,20 @@
 package br.unicap.nomedisciplina.atividade00;
 
+import org.junit.jupiter.api.Test;
+
+import br.unicap.nomedisciplina.atividade00.player.Jogador;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestJogador {
+
+
+@Test
+public void testGetNome(){
+    Jogador jogador = new Jogador("Jogador 1");
+    assertEquals("jogador 1", jogador.getNome());
+
+}
+
     
 }
