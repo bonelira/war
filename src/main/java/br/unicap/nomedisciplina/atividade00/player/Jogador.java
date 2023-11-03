9dbe00693b0c75f3f3fc8fc37fcbbba3dc43e7d5
Bruno Vicente <brunohiperbole@gmail.com>
2023-11-03 02:49:05 -0300

    Criando classe jogador

---
+++ src/main/java/br/unicap/nomedisciplina/atividade00/player/Jogador.java
@@ -0,0 +1,19 @@
+package br.unicap.nomedisciplina.atividade00.player;
+
+public class Jogador {
+    
+
+  
+    private String nome;
+
+
+    public Jogador (String nome){
+        this.nome = nome;
+    }
+
+
+    
+    public String getNome() {
+        return nome;
+    }
+}
