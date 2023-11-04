f3dee69ab1d9b69b77f46aaad24059ed75de7924
rooseveltjnr <101573032+rooseveltjnr@users.noreply.github.com>
2023-11-04 02:27:54 +0000

    create other classes card

--- src/main/java/br/unicap/eng2/debuggin_squad/war/Card/CardJoker.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/Card/CardJoker.java
@@ -1,5 +1,47 @@
 package br.unicap.eng2.debuggin_squad.war.Card;
 
-public class CardJoker {
+public class CardJoker extends Card {
+
+    private String name;
+    private String description;
+    private int number;
+    private String shape;
+
+    public CardJoker() {
+        this.name = "Joker";
+        this.description = "Joker card";
+        this.number = 0;
+        this.shape = "Joker";
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public int getNumber() {
+        return this.number;
+    }
+
+    @Override
+    public Object getType() {
+        return this;
+    }
+
+    @Override
+    public String getShape() {
+        return this.shape;
+    }
+
+    @Override
+    public String getObjective() {
+        return null;
+    }
     
 }
--- src/main/java/br/unicap/eng2/debuggin_squad/war/Card/CardTerritory.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/Card/CardTerritory.java
@@ -1,5 +1,69 @@
 package br.unicap.eng2.debuggin_squad.war.Card;
 
-public class CardTerritory {
+public class CardTerritory extends Card {
+
+    private String name;
+    private String description;
+    private int number;
+    private String type;
+    private String shape;
+    private String objective;
+
+    public CardTerritory() {
+        this.name = "Territory";
+        this.description = "Territory";
+        this.number = 1; // toda carta territorio comeca com 1 exercito
+        this.type = "Territory";
+        this.shape = "Territory";
+        this.objective = "Territory";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public String getObjective() {
+        return null;
+    }
+
+    public void setObjective(String objective) {
+        this.objective = objective;
+    }
     
 }
