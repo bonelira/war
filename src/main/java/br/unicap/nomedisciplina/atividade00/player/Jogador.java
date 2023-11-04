e36740d95dbe1b1f2669d531d5e404240dc4f8b0
Bruno Vicente <brunohiperbole@gmail.com>
2023-11-04 00:21:09 -0300

    Atualizando testes da classe jogador

--- src/main/java/br/unicap/nomedisciplina/atividade00/player/Jogador.java
+++ src/main/java/br/unicap/nomedisciplina/atividade00/player/Jogador.java
@@ -1,19 +1,70 @@
 package br.unicap.nomedisciplina.atividade00.player;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.unicap.nomedisciplina.atividade00.territory.Territorio;
+
 public class Jogador implements Players {
     
 
   
     private String nome;
+    private List<Territorio> territorios;
+    private int qtdExercitos;
 
 
     public Jogador (String nome){
         this.nome = nome;
+         this.territorios = new ArrayList<>(6);
+        this.qtdExercitos = 0;
+    }
+
+
+    public int getQtdExercitos() {
+        return qtdExercitos;
+    }
+
+
+
+    public void setQtdExercitos(int qtdExercitos) {
+        this.qtdExercitos = qtdExercitos;
     }
 
 
     
     public String getNome() {
         return nome;
+    }
+
+
+    public List<Territorio> getTerritorios() {
+        return territorios;
+    }
+
+
+    public void adicionarTerritorio(Territorio territorio) {
+        territorios.add(territorio);
+    }
+
+
+    public void removerTerritorio(Territorio territorio) {
+        territorios.remove(territorio);
+    }
+
+    public boolean possuiTerritorio(String nomeTerritorio) {
+        for (Territorio territorio : territorios) {
+            if (territorio.getNome().equals(nomeTerritorio)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void mostrarTerritorios() {
+        System.out.println("Territórios de " + nome + ":");
+        for (Territorio territorio : territorios) {
+            System.out.println(territorio.getNome());
+        }
     }
 }
--- src/test/java/br/unicap/nomedisciplina/atividade00/TestJogador.java
+++ src/test/java/br/unicap/nomedisciplina/atividade00/TestJogador.java
@@ -1,5 +1,43 @@
 package br.unicap.nomedisciplina.atividade00;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.unicap.nomedisciplina.atividade00.player.Jogador;
+import br.unicap.nomedisciplina.atividade00.territory.Territorio;
+
 public class TestJogador {
+
+    private Jogador jogador;
+
+
+
+
+    @BeforeEach
+    public void setUp(){
+        jogador = new Jogador("nome");
+    }
+
+    
+    @Test
+    public void Test01adicionarTerritorioDeveAdicionarNaLista() {
+        Jogador jogador = new Jogador("Jogador 1");
+        Territorio territorio = new Territorio("Territorio A");
+        jogador.adicionarTerritorio(territorio);
+        assertTrue(jogador.getTerritorios().contains(territorio));
+    }
+
+    @Test
+    public void removerTerritorioDeveRemoverDaLista() {
+        Jogador jogador = new Jogador("Jogador 2");
+        Territorio territorio = new Territorio("Territorio B");
+        jogador.adicionarTerritorio(territorio);
+        jogador.removerTerritorio(territorio);
+        assertFalse(jogador.getTerritorios().contains(territorio));
+    }
+
     
 }
