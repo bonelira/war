5180f16c3e75197f9216dcbe1a6afb62deaad633
Bone Lira <bone-lira@hotmail.com>
2023-11-03 23:57:28 -0300

    feat criando cosnt

--- src/main/java/br/unicap/eng2/debuggin_squad/war/GameConfigurator.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/GameConfigurator.java
@@ -4,13 +4,23 @@
 
 package br.unicap.eng2.debuggin_squad.war;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.unicap.eng2.debuggin_squad.war.controller.Player;
 
 public class GameConfigurator {
+    List<Player> players;
 
-    public GameConfigurator(int i, int j) {
+    public GameConfigurator(int qntd, int j) {
+        players = new ArrayList<>(); // Inicialize a lista de jogadores
+
+        for (int i = 0; i < qntd; i++) {
+            // Crie instâncias de jogadores e adicione à lista
+            Player player = new Player(null, null);
+            players.add(player);
+        }
+
     }
 
     public List<Player> getListOfPlayers() {
