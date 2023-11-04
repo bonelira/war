94155c919422b29c3ab7654f89d159ff24cb8207
Bone Lira <bone-lira@hotmail.com>
2023-11-03 23:22:19 -0300

    feat: add methods and improve GameConfigurator

--- src/main/java/br/unicap/eng2/debuggin_squad/war/GameConfigurator.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/GameConfigurator.java
@@ -4,6 +4,20 @@
 
 package br.unicap.eng2.debuggin_squad.war;
 
+import java.util.List;
+
+import br.unicap.eng2.debuggin_squad.war.controller.Player;
+
 public class GameConfigurator {
+
+    public GameConfigurator(int i, int j) {
+    }
+
+    public List<Player> getListOfPlayers() {
+        return null;
+    }
+
+    public void setNumberOfPlayers(int numberOfPlayers) {
+    }
 
 }
