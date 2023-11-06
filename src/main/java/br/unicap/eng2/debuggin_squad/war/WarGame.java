8e36a37380adf104b9c0883d2f0d56b2589dbe50
Bone Lira <bone-lira@hotmail.com>
2023-11-05 21:35:51 -0300

    FEAT: getplayer method

--- src/main/java/br/unicap/eng2/debuggin_squad/war/WarGame.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/WarGame.java
@@ -1,10 +1,16 @@
 package br.unicap.eng2.debuggin_squad.war;
 
+import br.unicap.eng2.debuggin_squad.war.controller.Player;
+
 public class WarGame {
 
     public Object getPlayer1;
 
     public void start() {
+    }
+
+    public Object getPlayer(Player player1) {
+        return null;
     }
 
 }
