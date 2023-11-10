84038551cb032190e76bf9e4bf04ce49de70fee7
leohcavalcanti <leonardocaturyty@hotmail.com>
2023-11-10 10:10:27 -0300

    feat: adding map and player imports

--- src/main/java/br/unicap/eng2/debuggin_squad/war/interfaces/IObjective.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/interfaces/IObjective.java
@@ -1,5 +1,9 @@
 package br.unicap.eng2.debuggin_squad.war.interfaces;
 
+// Altera as importações para o projeto final.
+import br.unicap.eng2.debuggin_squad.war.Map;
+import br.unicap.eng2.debuggin_squad.war.Player;
+
 public interface IObjective {
 
     public boolean objectiveAchieved(Map map, Player Player);
