f069a2e44a62a1c796761408ba0a3b53f455d431
Bone Lira <bone-lira@hotmail.com>
2023-11-12 00:59:11 -0300

    feature: specifying and add countries to map

--- src/main/java/br/unicap/eng2/debuggin_squad/war/Board.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/Board.java
@@ -17,9 +17,7 @@ public class Board {
 
         // Criando territórios
 
-        List<Territory> territories = new ArrayList<>();
-
-        // Europa
+        // Europa // territórios: 7
         Territory sweden = new Territory("Suécia", null);
         Territory germany = new Territory("Alemanha", null);
         Territory poland = new Territory("Polônia", null);
@@ -28,7 +26,7 @@ public class Board {
         Territory england = new Territory("Inglaterra", null);
         Territory iceland = new Territory("Islândia", null);
 
-        // África
+        // África // territórios: 6
         Territory algeria = new Territory("Argéria", null);
         Territory egypt = new Territory("Egito", null);
         Territory sudan = new Territory("Sudão", null);
@@ -36,7 +34,7 @@ public class Board {
         Territory madagascar = new Territory("Madagascar", null);
         Territory south_africa = new Territory("África do Sul", null);
 
-        // Ásia
+        // Ásia // territórios: 12
         Territory japan = new Territory("Japão", null);
         Territory vietnan = new Territory("Vietnã", null);
         Territory india = new Territory("Índia", null);
@@ -50,7 +48,7 @@ public class Board {
         Territory siberia = new Territory("Sibéria", null);
         Territory vladvostok = new Territory("Vladvostok", null);
 
-        // América do Norte
+        // América do Norte // territórios: 8
         Territory alaska = new Territory("Alasca", null);
         Territory mackenzie = new Territory("Mackenzie", null);
         Territory vancouver = new Territory("Vancouver", null);
@@ -60,19 +58,61 @@ public class Board {
         Territory mexico = new Territory("México", null);
         Territory california = new Territory("Califórnia", null);
 
-        // América do Sul
+        // América do Sul // territórios: 4
         Territory brasil = new Territory("Brasil", null);
         Territory peru = new Territory("Peru", null);
         Territory argentina = new Territory("Argentina", null);
         Territory venezuela = new Territory("Venezuela", null);
 
-        // Oceania
+        // Oceania // territórios: 4
         Territory australia = new Territory("Austrália", null);
         Territory sumatra = new Territory("Sumatra", null);
         Territory borneo = new Territory("Borneo", null);
         Territory new_guinea = new Territory("Nova Guiné", null);
 
-        
+        territories.add(sweden);
+        territories.add(germany);
+        territories.add(poland);
+        territories.add(moscow);
+        territories.add(france);
+        territories.add(england);
+        territories.add(iceland);
+        territories.add(algeria);
+        territories.add(egypt);
+        territories.add(sudan);
+        territories.add(congo);
+        territories.add(madagascar);
+        territories.add(south_africa);
+        territories.add(japan);
+        territories.add(vietnan);
+        territories.add(india);
+        territories.add(middle_east);
+        territories.add(aral);
+        territories.add(omsk);
+        territories.add(china);
+        territories.add(dudinka);
+        territories.add(tchita);
+        territories.add(mongolia);
+        territories.add(siberia);
+        territories.add(vladvostok);
+        territories.add(alaska);
+        territories.add(mackenzie);
+        territories.add(vancouver);
+        territories.add(ottawa);
+        territories.add(new_york);
+        territories.add(labrador);
+        territories.add(mexico);
+        territories.add(california);
+        territories.add(brasil);
+        territories.add(peru);
+        territories.add(argentina);
+        territories.add(venezuela);
+        territories.add(australia);
+        territories.add(sumatra);
+        territories.add(borneo);
+        territories.add(new_guinea);
+
+        return territories;
 
     }
 }
