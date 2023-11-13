c3567b09d51223d85feec31e0db5a1f8818be75f
Bone Lira <bone-lira@hotmail.com>
2023-11-12 23:10:54 -0300

    feat: initial config of class

--- src/main/java/br/unicap/eng2/debuggin_squad/war/GoalCard.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/GoalCard.java
@@ -2,8 +2,18 @@ package br.unicap.eng2.debuggin_squad.war;
 
 public class GoalCard {
 
+    private String goal;
+
     public GoalCard() {
 
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public String getGoal() {
+        return this.goal;
     }
 
 }
