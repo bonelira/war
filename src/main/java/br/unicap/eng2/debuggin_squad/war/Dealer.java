fcff315c66bcb9b5dc4b98302d0a5e8436f59825
Bone Lira <bone-lira@hotmail.com>
2023-11-12 23:34:03 -0300

    feat: dealer class

---
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/Dealer.java
@@ -0,0 +1,30 @@
+// @bonelira
+
+package br.unicap.eng2.debuggin_squad.war;
+
+import br.unicap.eng2.debuggin_squad.war.controller.Territory;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealer {
+    private List<Territory> deckTerritories;
+    private List<GoalCard> deckG;
+    private List<Territory> deckEmptyTerritories;
+
+    private void shuffleTerritoryCards() {
+
+    }
+
+    private void shuffleGoalCards() {
+
+    }
+
+    public void putGoalCardBackInDeck(GoalCard goalCard) {
+
+    }
+
+    public void putTerritoryCardBackInDeck(Territory territoryCard) {
+
+    }
+
+}
