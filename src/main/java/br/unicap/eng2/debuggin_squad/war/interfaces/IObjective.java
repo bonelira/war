6d6cd2fac06405725f23b51ca31d8d50cbbf27ad
Leonardo Cavalcanti <leonardocaturyty@hotmail.com>
2023-11-13 22:55:04 -0300

    style: Updates the code style

--- src/main/java/br/unicap/eng2/debuggin_squad/war/interfaces/IObjective.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/interfaces/IObjective.java
@@ -1,6 +1,5 @@
 package br.unicap.eng2.debuggin_squad.war.interfaces;
 
-// Altera as importações para o projeto final.
 import br.unicap.eng2.debuggin_squad.war.Player;
 import br.unicap.eng2.debuggin_squad.war.Territory;
 
