6f4cc6a3179e24f4c3ffafa436f2743982555a7c
Leonardo Cavalcanti <leonardocaturyty@hotmail.com>
2023-11-13 21:14:16 -0300

    refactor: by changing the Map class parameter to Territory.

--- src/main/java/br/unicap/eng2/debuggin_squad/war/interfaces/IObjective.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/interfaces/IObjective.java
@@ -1,10 +1,10 @@
 package br.unicap.eng2.debuggin_squad.war.interfaces;
 
 // Altera as importações para o projeto final.
-import br.unicap.eng2.debuggin_squad.war.Map;
 import br.unicap.eng2.debuggin_squad.war.Player;
+import br.unicap.eng2.debuggin_squad.war.Territory;
 
 public interface IObjective {
 
-    public boolean objectiveAchieved(Map map, Player Player);
+    public boolean objectiveAchieved(Territory territory, Player Player);
 }
