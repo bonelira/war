d7d25e723e6181fb399e1b880c09af395a38a91a
Bruno <brunoirae@gmail.com>
2023-11-28 20:56:09 -0300

    refactor: adjusting to receive parameters from the Initialization class

--- src/test/java/br/unicap/eng2/debuggin_squad/war/relocation/RelocationServiceTest.java
+++ src/test/java/br/unicap/eng2/debuggin_squad/war/relocation/RelocationServiceTest.java
@@ -1,9 +1,6 @@
-/**
- * @author Bruno Iraê <brunoirae@gmail.com / @brunoirae>
- */
-
 package br.unicap.eng2.debuggin_squad.war.relocation;
 
+import br.unicap.eng2.debuggin_squad.war.Initialization;
 import br.unicap.eng2.debuggin_squad.war.controller.Player;
 import br.unicap.eng2.debuggin_squad.war.controller.Territory;
 import br.unicap.eng2.debuggin_squad.war.model.state.relocation.RelocationContext;
@@ -12,17 +9,15 @@ import br.unicap.eng2.debuggin_squad.war.service.RelocationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class RelocationServiceTest {
+public class RelocationServiceTest extends Initialization {
     private Player player1;
-
-    private List<Territory> conqueredTerritories;
-    private Territory brasil;
-    private Territory argentina;
-    private Territory afeganistao;
-    private Territory uruguai;
+    private Territory territory;
+    private Territory territory2;
+    private Territory adjacent;
+    private Territory armies;
 
     private RelocationService realocar;
     private RelocationContext relocationContext;
@@ -31,50 +26,47 @@ public class RelocationServiceTest {
     public void setup() {
         realocar = new RelocationService();
         relocationContext = new RelocationContext();
-        conqueredTerritories = new ArrayList<Territory>();
     }
-
-
 
     @Test
     public void testValidRelocation() {
-        mockTerritoryAndArmy();
-        mockPlayer1Territory();
-        mockAdjacents();
+        player1 = initializePlayer();
+        initializeAdjacents();
+        territory = initializeTerritoryBrasil();
+        territory2 = initializeTerritoryArgentina();
         int armiesTransferido = 4;
 
-        realocar.relocationTroops(player1, brasil, argentina, armiesTransferido);
+        realocar.relocationTroops(player1, territory, territory2, armiesTransferido);
 
-        assertEquals(1, brasil.getArmiesCount() - armiesTransferido);
-        assertEquals(9, argentina.getArmiesCount() + armiesTransferido);
+        assertEquals(1, territory.getArmiesCount() - armiesTransferido);
+        assertEquals(9, territory2.getArmiesCount() + armiesTransferido);
     }
 
     @Test
-    public void testValidateErrorTerritoryNotAdjacent(){
-        mockTerritoryAndArmy();
-        mockPlayer1Territory();
-        mockAdjacents();
+    public void testValidateErrorTerritoryNotAdjacent() {
+        player1 = initializePlayer();
+        territory = initializeTerritoryBrasil();
+        initializeAdjacents();
         int armiesTransferido = 4;
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            realocar.relocationTroops(player1, brasil, afeganistao, armiesTransferido);
+            realocar.relocationTroops(player1, territory, adjacent, armiesTransferido);
         });
 
         assertEquals(RelocationNormalState.MSG_TERRITORY_NOT_ADJACENT, exception.getMessage());
     }
 
     @Test
-    public void testValidateErrorTerritoryNotConquered(){
-        mockTerritoryAndArmy();
-        mockPlayer1Territory();
-        mockAdjacents();
+    public void testValidateErrorTerritoryNotConquered() {
+        player1 = initializePlayer();
+        territory = initializeTerritoryBrasil();
+        territory2 = initializeTerritoryArgentina();
         int armiesTransferido = 4;
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            realocar.relocationTroops(player1, brasil, uruguai, armiesTransferido);
+            realocar.relocationTroops(player1, territory, territory2, armiesTransferido);
         });
 
         assertEquals(RelocationNormalState.MSG_TERRITORY_NOT_CONQUERED, exception.getMessage());
     }
 }
-
