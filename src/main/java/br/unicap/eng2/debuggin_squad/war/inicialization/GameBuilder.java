530b4d94ddef72439a35c747f75e6eea162f5667
Bone Lira <bone-lira@hotmail.com>
2023-11-30 07:56:48 -0300

    feat: GameBuilder class

--- src/main/java/br/unicap/eng2/debuggin_squad/war/inicialization/GameBuilder.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/inicialization/GameBuilder.java
@@ -1,43 +1,80 @@
+/**
+ * @author Abi-albom Lira <bone-lira@hotmail.com / @bonelira>
+ */
+
 package br.unicap.eng2.debuggin_squad.war.inicialization;
 
-import br.unicap.eng2.debuggin_squad.war.WarGame;
+import java.util.List;
+import java.util.ArrayList;
+
+import br.unicap.eng2.debuggin_squad.war.Board;
+import br.unicap.eng2.debuggin_squad.war.GoalCard;
+import br.unicap.eng2.debuggin_squad.war.controller.Player;
+import br.unicap.eng2.debuggin_squad.war.controller.Territory;
 
 public class GameBuilder implements Builder {
     public GameBuilder() {
         this.game = new WarGame();
     }
 
-    private WarGame game;
     // implementar métodos para configurar alguns aspectos do jogo
     // durante a inicialização (tabuleiro, jogadores, cartas, regras etc)
     // métodos para inicialização das Rules (Buildar ele)
 
+    private WarGame game;
+    private Board board;
+    private List<Player> players;
+    private List<GoalCard> goalCards;
+    private int playersQuantity;
+    private List<Territory> territories;
+
+    @Override
+    public void configureBoard(List<Territory> territories) {
+        game.setTerritoriesList(territories);
+
+        List<Player> playerOwner = new ArrayList();
+        game.getPlayers().forEach(player -> playerOwner.add(player));
+
+        int i = 0;
+        for (Territory territory : territories) {
+            territory.setProprietario(playerOwner.get(0));
+
+            i++;
+        }
+
+    }
+
+    @Override
+    public void configurePlayers(List<Player> players) {
+        GoalCard goalCard = new GoalCard("Correr");
+        int i = 0;
+        for (Player player : players) {
+            // player.setName("Black" + i);
+            // player.setId("Black" + i);
+            player.setGoalCard(goalCard);
+            i++;
+        }
+
+        game.setPlayers(players);
+    }
+
+    @Override
+    public void configureCards(List<GoalCard> goalCards) {
+        for (GoalCard goalCard : goalCards) {
+            goalCard.setGoal("Correr");
+        }
+        game.setGoalCards(goalCards);
+    }
+
     // métodos para inicialização do Board (Buildar ele)
-    @Override
-    public GameBuilder boardConfiguration() {
-        // TODO Auto-generated method stub
-        return this;
-    }
 
-    // métodos para inicialização do Player (Buildar ele)
-    @Override
-    public GameBuilder playerConfiguration() {
-        // TODO Auto-generated method stub
-        return this;
-
-    }
-
-    // métodos para inicialização do Deck / Cards (Buildar ele)
-    @Override
-    public GameBuilder dealCards() {
-        // TODO Auto-generated method stub
-        return this;
-    }
-
-    @Override
-    public WarGame build() {
-        // simplesmente envia a instância do jogo construída aqui.
+    public WarGame getResult() {
         return this.game;
+    }
+
+    @Override
+    public void reset() {
+        this.game = new WarGame();
     }
 
 }
