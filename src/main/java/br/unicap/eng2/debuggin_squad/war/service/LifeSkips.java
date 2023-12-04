69f1b1fc00a58d276fd9325bc6caa6a4bbeb9dfa
Igor Freire Tenório Cavalcanti <igor.2019201200@unicap.br>
2023-12-04 01:39:46 -0300

    Update LifeSkips.java

--- src/main/java/br/unicap/eng2/debuggin_squad/war/service/LifeSkips.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/service/LifeSkips.java
@@ -3,17 +3,17 @@ package br.unicap.eng2.debuggin_squad.war.service;
 public class LifeSkips {
 
 	public void alivePlayers() {
-		P1.life;
+	/*	P1.life;
 		P2.life;
 		P3.life;
-		P4.life;
+		P4.life;*/
 	}
 
 	public void deadPlayers(){
-		P1.life;
+	/*	P1.life;
 		P2.life;
 		P3.life;
-		P4.life;
+		P4.life;*/
 	}
 
 	static String messageNotImplemented() {
