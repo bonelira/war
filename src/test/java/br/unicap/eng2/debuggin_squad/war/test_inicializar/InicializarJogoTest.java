fd68fc38b1568b764ebb3debce3ebc2cf39a4841
Bone Lira <bone-lira@hotmail.com>
2023-12-04 00:43:58 -0300

    refactor: tests and add a sugestion tests list

--- src/test/java/br/unicap/eng2/debuggin_squad/war/test_inicializar/InicializarJogoTest.java
+++ src/test/java/br/unicap/eng2/debuggin_squad/war/test_inicializar/InicializarJogoTest.java
@@ -3,6 +3,7 @@
  */
 package br.unicap.eng2.debuggin_squad.war.test_inicializar;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -112,16 +113,6 @@ public class InicializarJogoTest {
     // [ ] Teste inicialização: Board
     // [ ] Teste inicialização: Player
     // [ ] Teste inicialização: Dealer
-    // [ ] Teste inicialização: RuleManager
-    // [ ] Teste inicialização: GameConfigurator
-
-    // List<Player> players = configurator.getListOfPlayers();
-    // String cor1 = players.get(0).getId();
-    // String cor2 = players.get(1).getId();
-    // String cor3 = players.get(2).getId();
-    // String cor4 = players.get(3).getId();
-    // String cor5 = players.get(4).getId();
-    // String cor6 = players.get(5).getId();
 
     // assertEquals("Red", 0);
     // assertEquals("Blue", 1);
@@ -130,9 +121,149 @@ public class InicializarJogoTest {
     // assertEquals("Orange", 4);
     // assertEquals("Black", 5);
 
-    // }
+    // lista de testes para as inicializações:
+    /*
+     * # Board
+     * Territórios Criados:
+     * Verificar se todos os territórios foram corretamente criados.
+     * Certificar-se de que cada território tem um nome e uma localização válidos.
+     * Testar se os territórios foram atribuídos aos continentes corretos.
+     * Adjacências entre Territórios:
+     * Garantir que as adjacências entre os territórios estão corretas.
+     * Testar se os territórios adjacentes foram devidamente definidos para cada
+     * território.
+     * Verificar se as adjacências são bidirecionais (se A é adjacente a B, então B
+     * deve ser adjacente a A).
+     * Validação de Proprietários:
+     * Testar se todos os territórios têm um proprietário válido atribuído (se
+     * necessário para as regras do jogo).
+     * Atribuição Correta de Continentes:
+     * Certificar-se de que os territórios foram atribuídos aos continentes corretos
+     * de acordo com as regras do jogo.
+     * Verificar se todos os territórios pertencem a um continente válido.
+     * # GoalCard
+     * Distribuição correta de cartas:
+     * Teste se o número total de cartas distribuídas corresponde ao número esperado
+     * de jogadores.
+     * Verifique se cada jogador recebe exatamente uma carta de objetivo.
+     * Unicidade das cartas:
+     * Garanta que nenhuma carta de objetivo seja duplicada ou repetida entre os
+     * jogadores.
+     * Validação dos objetivos:
+     * Verifique se os objetivos são válidos de acordo com as regras do jogo, como
+     * conquistar um certo número de territórios, controlar continentes específicos,
+     * ou eliminar um jogador.
+     * Atribuição correta aos jogadores:
+     * Teste se as cartas de objetivo são atribuídas aos jogadores corretamente,
+     * assegurando que cada jogador tenha uma carta distinta.
+     * # Player
+     * Criação de Jogadores:
+     * Teste se a quantidade de jogadores criados corresponde ao número esperado.
+     * Verifique se cada jogador possui um nome único.
+     * Garanta que cada jogador tenha uma cor associada.
+     * Atribuição de Objetivos:
+     * Certifique-se de que cada jogador recebe um objetivo distinto e válido, de
+     * acordo com as regras do jogo.
+     * Validação de Cores:
+     * Teste se as cores dos jogadores são atribuídas corretamente, sem repetições
+     * ou conflitos de cores.
+     * Correspondência de Nomes:
+     * Verifique se os nomes atribuídos aos jogadores são não nulos e
+     * significativos.
+     * Integridade dos Jogadores:
+     * Garanta que não haja jogadores duplicados ou ausentes na inicialização.
+     * Adequação dos objetivos aos jogadores:
+     * Verifique se os objetivos atribuídos são razoáveis para a o jogador (jogador
+     * não pode destruir a si mesmo ou jogador não pode eliminar jogador que não
+     * esteja na partida).
+     */
 
-    // testes das inicializações:
+    @Test
+    public void testBoardInitializationIsNotNull() {
+        Director director = new Director();
+        GameBuilder builder = new GameBuilder();
+        director.constructDefaultGame(builder);
+        WarGame game = builder.getResult();
+
+        Board map = game.getBoard();
+        assertNotNull(map);
+
+    }
+
+    @Test
+    public void testBoardEachTerritoryHasNameAndContinentNotNullValues() {
+        Director director = new Director();
+        GameBuilder builder = new GameBuilder();
+        director.constructDefaultGame(builder);
+        WarGame game = builder.getResult();
+
+        List<Territory> territories = game.getTerritoriesList();
+        boolean nameIsInitialized = true;
+        boolean continentIsInitialized = true;
+
+        for (Territory territory : territories) {
+            if (territory.getNome().isEmpty()) {
+                nameIsInitialized = false;
+                return;
+            } else if (territory.getContinent().isEmpty()) {
+                continentIsInitialized = false;
+                return;
+            }
+        }
+
+        assertTrue(continentIsInitialized && nameIsInitialized);
+
+    }
+
+    @Test
+    public void testBoardTotalCountriesInOceaniaContinent() {
+        Director director = new Director();
+        GameBuilder builder = new GameBuilder();
+        director.constructDefaultGame(builder);
+        WarGame game = builder.getResult();
+
+        List<Territory> oceaniaTerritories = game.getBoard().getContinentCountriesList("Oceania");
+        int expected = 4;
+        int oceaniaTerritoriesSize = oceaniaTerritories.size();
+
+        assertEquals(expected, oceaniaTerritoriesSize);
+
+    }
+
+    @Test
+    public void testBoardNumberOfTerritories() {
+        Director director = new Director();
+        GameBuilder builder = new GameBuilder();
+        director.constructDefaultGame(builder);
+        WarGame game = builder.getResult();
+
+        int expected = 42;
+        int actual = game.getTerritoriesList().size();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBoarHasCorrectAdjacency() {
+        Director director = new Director();
+        GameBuilder builder = new GameBuilder();
+        director.constructDefaultGame(builder);
+        WarGame game = builder.getResult();
+
+        List<Territory> map = game.getTerritoriesList();
+
+        // Suécia --> Inglaterra
+        // se n funcionar é pq a contagem do index está errada (considerando que o
+        // método está correto)
+
+        int indexTerritoryA = 34;
+        int indexTerritoryB = 36;
+
+        // Verificar se as adjacências são bidirecionais (se A é adjacente a B, então B
+        // deve ser adjacente a A).
+        assertTrue(map.get(indexTerritoryA).isAdjacent(map.get(indexTerritoryB))
+                && map.get(indexTerritoryB).isAdjacent(map.get(indexTerritoryA)));
+    }
 
     @Test
     public void testPlayersInitialization() {
@@ -158,44 +289,6 @@ public class InicializarJogoTest {
         // assertNotNull(game.getPlayers().get(0)); // para verificar individualmente se
         // um player (.get(index do player)) foi criado.
 
-    }
-
-    @Test
-    public void testBoardInitializationIsNotNull() {
-        Director director = new Director();
-        GameBuilder builder = new GameBuilder();
-        director.constructDefaultGame(builder);
-        WarGame game = builder.getResult();
-
-        Board map = game.getBoard();
-        assertNotNull(map);
-
-    }
-
-    @Test
-    public void testBoardNumberOfTerritories() {
-        Director director = new Director();
-        GameBuilder builder = new GameBuilder();
-        director.constructDefaultGame(builder);
-        WarGame game = builder.getResult();
-
-        assertNotNull(game.getTerritoriesList());
-        assertEquals(42, game.getTerritoriesList().size());
-    }
-
-    @Test
-    public void testBoarHasCorrectAdjacency() {
-        Director director = new Director();
-        GameBuilder builder = new GameBuilder();
-        director.constructDefaultGame(builder);
-        WarGame game = builder.getResult();
-
-        List<Territory> map = game.getTerritoriesList();
-
-        // Suécia --> Inglaterra
-        // se n funcionar é pq a contagem do index está errada (considerando que o
-        // método está correto)
-        assertTrue(map.get(34).isAdjacent(map.get(36)));
     }
 
     @Test
