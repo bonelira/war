75eacb4a5196cb4b2d78ac86f39c6387b56939d2
Igor Freire <gamerdokebu@gmail.com>
2023-12-04 02:07:31 -0300

    correções auto grade + test goalkillplayer

--- src/main/java/br/unicap/eng2/debuggin_squad/war/controller/Life.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/controller/Life.java
@@ -1,15 +1,22 @@
 package br.unicap.eng2.debuggin_squad.war.controller;
 
+import java.util.List;
+
 public class Life {
-    private List<Territory> conqueredTerritories;
+    private static List<Territory> conqueredTerritories;
     private Life life;
     private String id;
 
     public static boolean isAlive(){
-        if (conqueredTerritories <= 0){
+        return false;
+        /*if (conqueredTerritories <= 0){
             return true;
         } else {
             return false;
-        }
+        }*/
+    }
+
+    public static String messageNotImplemented() {
+        return null;
     }
 }
--- src/test/java/br/unicap/eng2/debuggin_squad/war/PlayersLife.java
+++ src/test/java/br/unicap/eng2/debuggin_squad/war/PlayersLife.java
@@ -4,12 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import br.unicap.eng2.debuggin_squad.war.controller.Life;
+
 public class PlayersLife {
 
   @Test
   public void testAlivePlayersMoreThanPlayingPlayers() {
     String expected = "There are more alive players than players registered, my code must be garbage";
-    String actual = Life.messageImplemented();
+    String actual = Life.messageNotImplemented();
     assertEquals(expected, actual);
   }
 
