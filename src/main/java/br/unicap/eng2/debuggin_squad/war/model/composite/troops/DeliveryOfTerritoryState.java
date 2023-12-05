5f066c230fee69b602520715e337018585d986f3
Bruno <brunoirae@gmail.com>
2023-12-05 11:36:44 -0300

    chore: adapted class DeliveryOfTerritoryState for pattern Observer

--- src/main/java/br/unicap/eng2/debuggin_squad/war/model/composite/troops/DeliveryOfTerritoryState.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/model/composite/troops/DeliveryOfTerritoryState.java
@@ -7,12 +7,15 @@ package br.unicap.eng2.debuggin_squad.war.model.composite.troops;
 import br.unicap.eng2.debuggin_squad.war.controller.Player;
 import br.unicap.eng2.debuggin_squad.war.controller.Territory;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class DeliveryOfTerritoryState implements TroopsComponent{
+public class DeliveryOfTerritoryState implements TroopsComponent {
     private TroopsComponent currentState;
+    private List<Observer> observers = new ArrayList<>();
+
     public static final String MSG_ERROR_TROOPS = "Delivery troops failed.";
-    public static final String MSG_TERRITORY_DID_NOT_CONQUER = "The player not did not conquer territories";
+    public static final String MSG_TERRITORY_DID_NOT_CONQUER = "The player did not conquer territories";
 
     @Override
     public int deliverArmies(List<Player> players) {
@@ -22,10 +25,39 @@ public class DeliveryOfTerritoryState implements TroopsComponent{
         if (validateTroops(player)) {
             int totalTerritories = conqueredTerritories.size();
             int armiesReceived = totalTerritories / 2;
+            notifyObservers();
             return armiesReceived;
         } else {
             throw new IllegalArgumentException(MSG_ERROR_TROOPS);
         }
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        int totalTroops = calculateTotalTroops();
+        for (Observer observer : observers) {
+            observer.update(totalTroops);
+        }
+    }
+
+    private int calculateTotalTroops() {
+        return 0;
+    }
+
+
+    @Override
+    public int getTroopCount() {
+        return 0;
     }
 
     public boolean validateTroops(Player player) {
@@ -36,6 +68,8 @@ public class DeliveryOfTerritoryState implements TroopsComponent{
         return true;
     }
 
+    @Override
+    public void update(int newTroopCount) {
 
-    public void setState(TroopsComponent state) {this.currentState = state;}
+    }
 }
