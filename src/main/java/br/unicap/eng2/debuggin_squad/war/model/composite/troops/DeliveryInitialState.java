abcf1a45788e20a75df9796996f03ba2dd1fb0ce
Bruno <brunoirae@gmail.com>
2023-12-05 11:36:34 -0300

    chore: adapted class DeliveryInitialState for pattern Observer

--- src/main/java/br/unicap/eng2/debuggin_squad/war/model/composite/troops/DeliveryInitialState.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/model/composite/troops/DeliveryInitialState.java
@@ -6,30 +6,57 @@ package br.unicap.eng2.debuggin_squad.war.model.composite.troops;
 
 import br.unicap.eng2.debuggin_squad.war.controller.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class DeliveryInitialState implements TroopsComponent{
+public class DeliveryInitialState implements TroopsComponent {
     private TroopsComponent currentState;
+    private List<Observer> observers = new ArrayList<>();
+
     public static final String MSG_ERROR_TROOPS = "Delivery troops failed.";
     public static final String MSG_TERRITORY_EMPTY = "The territory cannot be empty";
 
     @Override
     public int deliverArmies(List<Player> players) {
         if (validateTroopsInitial(players)) {
-            int exercitosRecebidos = 42 / players.size();
-            return exercitosRecebidos;
+            int armiesReceived = 42 / players.size();
+            notifyObservers();
+            return armiesReceived;
         } else {
             throw new IllegalArgumentException(MSG_ERROR_TROOPS);
         }
     }
 
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        int totalTroops = 42;
+        for (Observer observer : observers) {
+            observer.update(totalTroops);
+        }
+    }
+
+    @Override
+    public int getTroopCount() {
+        return 0;
+    }
+
     public boolean validateTroopsInitial(List<Player> players) {
-        if (players.size() <= 0 ) {
+        if (players.size() <= 0) {
             throw new IllegalArgumentException(MSG_TERRITORY_EMPTY);
         }
         return true;
     }
 
-
-    public void setState(TroopsComponent state) {this.currentState = state;}
+    @Override
+    public void update(int newTroopCount) {}
 }
