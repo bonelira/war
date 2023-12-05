30ce3f10395729139931053eedd4deae0cf96ca6
Bruno <brunoirae@gmail.com>
2023-12-05 11:36:24 -0300

    chore: adapted class CompositeTroop for pattern Observer

--- src/main/java/br/unicap/eng2/debuggin_squad/war/model/composite/troops/CompositeTroop.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/model/composite/troops/CompositeTroop.java
@@ -6,10 +6,12 @@ package br.unicap.eng2.debuggin_squad.war.model.composite.troops;
 
 import br.unicap.eng2.debuggin_squad.war.controller.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
-class CompositeTroop implements TroopsComponent {
-    private List<TroopsComponent> troops;
+public class CompositeTroop implements TroopsComponent {
+    private List<TroopsComponent> troops = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
 
     public CompositeTroop(List<TroopsComponent> troops) {
         this.troops = troops;
@@ -21,7 +23,36 @@ class CompositeTroop implements TroopsComponent {
         for (TroopsComponent troop : troops) {
             totalArmies += troop.deliverArmies(players);
         }
+        notifyObservers();
         return totalArmies;
     }
 
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        int totalTroops = getTroopCount();
+        for (Observer observer : observers) {
+            observer.update(totalTroops);
+        }
+    }
+
+    public int getTroopCount() {
+        int totalTroops = 0;
+        for (TroopsComponent troop : troops) {
+            totalTroops += troop.getTroopCount();
+        }
+        return totalTroops;
+    }
+
+    @Override
+    public void update(int newTroopCount) {}
 }
