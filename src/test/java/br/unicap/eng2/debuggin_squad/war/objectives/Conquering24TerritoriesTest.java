a84ea7630faa0ced29e59b0943a76f0872d689b8
Leonardo Cavalcanti <leonardocaturyty@hotmail.com>
2023-12-04 21:50:08 -0300

    test: deletes methods from the test class

--- src/test/java/br/unicap/eng2/debuggin_squad/war/objectives/Conquering24TerritoriesTest.java
+++ src/test/java/br/unicap/eng2/debuggin_squad/war/objectives/Conquering24TerritoriesTest.java
@@ -1,9 +1,8 @@
 package br.unicap.eng2.debuggin_squad.war.objectives;
 
-
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Conquering24TerritoriesTest {
 
