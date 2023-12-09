104a225396b687556d11e101ab093721af15072e
Bruno <brunoirae@gmail.com>
2023-12-09 14:28:09 -0300

    refactor: adjusted fortification phase to receive a list of players

--- src/main/java/br/unicap/eng2/debuggin_squad/war/model/state/fortify/FortifyState.java
+++ src/main/java/br/unicap/eng2/debuggin_squad/war/model/state/fortify/FortifyState.java
@@ -7,8 +7,10 @@ package br.unicap.eng2.debuggin_squad.war.model.state.fortify;
 import br.unicap.eng2.debuggin_squad.war.controller.Player;
 import br.unicap.eng2.debuggin_squad.war.controller.Territory;
 
+import java.util.List;
+
 public interface FortifyState {
-    void fortifyArmies(Player player, int armies, Territory origin);
-    boolean validateFortification(Player player, int armies, Territory origin);
+    void fortifyArmies(List<Player> players, int armies, Territory origin);
+    boolean validateFortification(List<Player> players, int armies, Territory origin);
     void setState(FortifyState state);
 }
