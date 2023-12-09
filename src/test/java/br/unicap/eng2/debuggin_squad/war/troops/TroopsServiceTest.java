280b4de98e23b0546a1e43a6f43acdf93fb92094
Bruno <brunoirae@gmail.com>
2023-12-09 10:27:00 -0300

    test: created test for alocation troop in continent Africa

--- src/test/java/br/unicap/eng2/debuggin_squad/war/troops/TroopsServiceTest.java
+++ src/test/java/br/unicap/eng2/debuggin_squad/war/troops/TroopsServiceTest.java
@@ -6,6 +6,7 @@ package br.unicap.eng2.debuggin_squad.war.troops;
 
 import br.unicap.eng2.debuggin_squad.war.controller.Player;
 import br.unicap.eng2.debuggin_squad.war.controller.Territory;
+import br.unicap.eng2.debuggin_squad.war.model.composite.troops.ContinentTroopDelivery;
 import br.unicap.eng2.debuggin_squad.war.model.composite.troops.DeliveryInitialState;
 import br.unicap.eng2.debuggin_squad.war.model.composite.troops.DeliveryOfTerritoryState;
 import br.unicap.eng2.debuggin_squad.war.service.TroopsService;
@@ -67,6 +68,42 @@ public class TroopsServiceTest {
         when(player1.getConqueredTerritories()).thenReturn(conqueredTerritories);
     }
 
+    private void mockAfricanTerritories() {
+        Territory algeria = mock(Territory.class);
+        when(algeria.getName()).thenReturn("Algeria");
+        when(algeria.getContinent()).thenReturn("Africa");
+
+        Territory egypt = mock(Territory.class);
+        when(egypt.getName()).thenReturn("Egypt");
+        when(egypt.getContinent()).thenReturn("Africa");
+
+        Territory sudan = mock(Territory.class);
+        when(sudan.getName()).thenReturn("Sudan");
+        when(sudan.getContinent()).thenReturn("Africa");
+
+        Territory congo = mock(Territory.class);
+        when(congo.getName()).thenReturn("Congo");
+        when(congo.getContinent()).thenReturn("Africa");
+
+        Territory madagascar = mock(Territory.class);
+        when(madagascar.getName()).thenReturn("Madagascar");
+        when(madagascar.getContinent()).thenReturn("Africa");
+
+        Territory southAfrica = mock(Territory.class);
+        when(southAfrica.getName()).thenReturn("South Africa");
+        when(southAfrica.getContinent()).thenReturn("Africa");
+
+        List<Territory> africanTerritories = new ArrayList<>();
+        africanTerritories.add(algeria);
+        africanTerritories.add(egypt);
+        africanTerritories.add(sudan);
+        africanTerritories.add(congo);
+        africanTerritories.add(madagascar);
+        africanTerritories.add(southAfrica);
+
+        when(player1.getConqueredTerritories()).thenReturn(africanTerritories);
+    }
+
     @Test
     public void testValidatesTroopsSentInTheInitialTurn() {
         mockPlayers();
@@ -92,8 +129,18 @@ public class TroopsServiceTest {
         assertEquals(expectedExercitosRecebidos, exercitosRecebidos);
     }
 
-    //Criar teste para alocação de exercitos através da conquista de continente
-    /*Criar um método mock para alocar os continentes*/
+    @Test
+    public void testValidatesTroopsSentInContinentAfrica() {
+        mockPlayers();
+        mockAfricanTerritories();
+        ContinentTroopDelivery continentTroopDelivery = new ContinentTroopDelivery();
+
+        int armiesReceived = continentTroopDelivery.deliverArmies(players);
+        int expectedArmiesReceived = 3;
+
+        assertEquals(expectedArmiesReceived, armiesReceived);
+    }
+
 
     //Criar teste para alocação de exercitos através da troca de carta
     /*Criar um método mock para alocar as cartas*/
