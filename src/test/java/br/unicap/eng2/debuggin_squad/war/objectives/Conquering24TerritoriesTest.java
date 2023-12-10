fd696a9f38598a6c39be9bf926868f6155799673
Leonardo Cavalcanti <leonardocaturyty@hotmail.com>
2023-12-10 02:11:25 -0300

    feat: adds an author to show the creator of the class

--- src/test/java/br/unicap/eng2/debuggin_squad/war/objectives/Conquering24TerritoriesTest.java
+++ src/test/java/br/unicap/eng2/debuggin_squad/war/objectives/Conquering24TerritoriesTest.java
@@ -1,3 +1,7 @@
+/**
+ * @author Leonardo Cavalcanti <leonardocaturyty@gmail.com>
+ */
+
 package br.unicap.eng2.debuggin_squad.war.objectives;
 
 import br.unicap.eng2.debuggin_squad.war.controller.Player;
